40388af63d24ddbdde89647ae297c3b4413a003a
BrokenMesh <mineking61442@gmail.com>
2024-12-07 12:53:33 +0100

    Layout and Baselogic for Home

---
+++ Password-Generator-V2/Logic/Models.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace Password_Generator_V2.Logic
+{
+    public class ServiceItem {
+        public ServiceItem(string name) {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+
+    }
+
+}

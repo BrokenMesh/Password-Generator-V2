9b11c0adc851bcfe0faabf1617c68792d6323511
El-Kord Hicham <elkordhicham@gmail.com>
2024-12-08 10:10:31 +0100

    Services and base Storag Logic

---
+++ Password-Generator-V2/Logic/AppStateService.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.NetworkInformation;
+
+namespace Password_Generator_V2.Logic
+{
+    public class AppStateService
+    {
+        private readonly StorageService storageService;
+        
+        public List<ServiceItem> ServiceItems;
+        
+        private string ServiceItemsKey = "ServiceItems";
+
+        public AppStateService(StorageService _storageService) {
+            storageService = _storageService;
+
+            ServiceItems = new List<ServiceItem>();
+            Task.Run(LoadServiceItems);
+        }
+
+        public void AddService(string _name) {
+            ServiceItems.Add(new ServiceItem(_name));
+            Task.Run(StoreServiceItems);
+        }
+
+        private async Task StoreServiceItems() {
+            await storageService.SetItemAsync(ServiceItemsKey, ServiceItems);
+        }
+
+        private async Task LoadServiceItems() {
+            var storedItems = await storageService.GetItemAsync<List<ServiceItem>>(ServiceItemsKey);
+            if (storedItems != null) {
+                ServiceItems = storedItems!;
+            }
+        }
+    }
+}
--- Password-Generator-V2/Program.cs
+++ Password-Generator-V2/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Password_Generator_V2.Logic;
 
 namespace Password_Generator_V2
 {
@@ -12,6 +13,7 @@ namespace Password_Generator_V2
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(typeof(AppStateService));
 
             await builder.Build().RunAsync();
         }

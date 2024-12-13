1c835638bd10ee6c8508c08e0100758e6ee95f72
El-Kord Hicham <elkordhicham@gmail.com>
2024-12-14 00:59:00 +0100

    Small Optimidsations

--- Password-Generator-V2/Logic/StorageService.cs
+++ Password-Generator-V2/Logic/StorageService.cs
@@ -11,25 +11,21 @@ namespace Password_Generator_V2.Logic
             jsRuntime = _jSRuntime;
         }
 
-        public async Task SetItemAsync<T>(string key, T value)
-        {
+        public async Task SetItemAsync<T>(string key, T value) {
             var jsonValue = JsonSerializer.Serialize(value);
             await jsRuntime.InvokeVoidAsync("localStorage.setItem", key, jsonValue);
         }
 
-        public async Task<T?> GetItemAsync<T>(string key)
-        {
+        public async Task<T?> GetItemAsync<T>(string key) {
             var jsonValue = await jsRuntime.InvokeAsync<string>("localStorage.getItem", key);
             return jsonValue is not null ? JsonSerializer.Deserialize<T>(jsonValue) : default;
         }
 
-        public async Task RemoveItemAsync(string key)
-        {
+        public async Task RemoveItemAsync(string key) {
             await jsRuntime.InvokeVoidAsync("localStorage.removeItem", key);
         }
 
-        public async Task ClearAsync()
-        {
+        public async Task ClearAsync() {
             await jsRuntime.InvokeVoidAsync("localStorage.clear");
         }
     }

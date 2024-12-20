791684efabee6ad31a3483d1f81ba5ec9ab3c54a
BrokenMesh <mineking61442@gmail.com>
2024-12-20 18:29:07 +0100

    Update to HttpClient

--- Password-Generator-V2/Program.cs
+++ Password-Generator-V2/Program.cs
@@ -13,7 +13,7 @@ namespace Password_Generator_V2
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress + "/Password-Generator-V2/") });
             builder.Services.AddScoped(typeof(StorageService));
             builder.Services.AddScoped(typeof(AppStateService));
 
